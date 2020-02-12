d3d2d67c793baceb4545a2c20dfe210ea56d3817
Martin Mallet <martinmallet32@gmail.com>
2020-02-11 20:36:21 -0400

    Nav Drawer Added

--- QuizTake2/app/src/main/java/com/learning/quiztake2/MainActivity.kt
+++ QuizTake2/app/src/main/java/com/learning/quiztake2/MainActivity.kt
@@ -2,21 +2,31 @@ package com.learning.quiztake2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.learning.quiztake2.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        val navController = this.findNavController(R.id.navHostFragment)
+        navController = this.findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        NavigationUI.setupWithNavController(
+            binding.navView,
+            navController
+        )
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.navHostFragment)
         return navController.navigateUp()
     }
 }
