7768f445f71dea9d4da6b571f2734f083d457031
Martin Mallet <martinmallet32@gmail.com>
2020-02-11 20:42:33 -0400

    hamburger added

--- QuizTake2/app/src/main/java/com/learning/quiztake2/MainActivity.kt
+++ QuizTake2/app/src/main/java/com/learning/quiztake2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.learning.quiztake2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -12,13 +13,16 @@ import com.learning.quiztake2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         navController = this.findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        drawerLayout = binding.drawerLayout
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
         NavigationUI.setupWithNavController(
             binding.navView,
@@ -27,6 +31,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
